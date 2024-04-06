636b91086dc5ccf3132c6eb0f38fa1653f2f9457
Tim Mavrin <tmavrin@cedaron.com>
2024-04-06 13:38:58 -0700

    Added orientation events Pitch/Roll/Yaw on screen Added center reference as an offset Drew a canvas on screen

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
+class GyroReference {
+  alpha: number = 0;
+  beta: number = 0;
+  gamma: number = 0;
+
+  constructor() {
+    this.calibrateCenter();
+  }
+
+  calibrateCenter() {
+    window.addEventListener(
+      'deviceorientation',
+      (e: DeviceOrientationEvent) => {
+        this.alpha = e.alpha ?? 0;
+        this.beta = e.beta ?? 0;
+        this.gamma = e.gamma ?? 0;
+      },
+      { once: true }
+    );
+  }
+}
+
+type VirtualKeyboard = {
+  boundingRect: Readonly<DOMRect>;
+  overlaysContent: boolean;
+  show: () => void;
+  hide: () => void;
+};
+
+const _gyroRef = new GyroReference();
 window.onload = () => {
   if (window.DeviceOrientationEvent) {
     window.addEventListener(
@@ -7,10 +37,10 @@ window.onload = () => {
         let beta = document.getElementById('beta');
         let gamma = document.getElementById('gamma');
 
-        if (alpha && beta && gamma) {
-          alpha.innerText = e.alpha?.toString() ?? '';
-          beta.innerText = e.beta?.toString() ?? '';
-          gamma.innerText = e.gamma?.toString() ?? '';
+        if (alpha && beta && gamma && e.alpha && e.beta && e.gamma) {
+          alpha.innerText = e.alpha - _gyroRef.alpha + '';
+          beta.innerText = e.beta - _gyroRef.beta + '';
+          gamma.innerText = e.gamma - _gyroRef.gamma + '';
         }
       },
       false
