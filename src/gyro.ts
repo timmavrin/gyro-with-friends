fd4cca3eee2ba155ef2d08f1a27213d4ca51931b
Tim Mavrin <tmavrin@cedaron.com>
2024-04-06 13:56:15 -0700

    cleaned up code. Added https so gyro works

---
+++ src/gyro.ts
@@ -0,0 +1,21 @@
+export class Gyro {
+  alpha: number = 0;
+  beta: number = 0;
+  gamma: number = 0;
+
+  constructor() {
+    this.calibrateCenter();
+  }
+
+  calibrateCenter() {
+    window.addEventListener(
+      'deviceorientation',
+      (e: DeviceOrientationEvent) => {
+        this.alpha = e.alpha ?? 0;
+        this.beta = e.beta ?? 0;
+        this.gamma = e.gamma ?? 0;
+      },
+      { once: true }
+    );
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,23 @@
-class GyroReference {
-  alpha: number = 0;
-  beta: number = 0;
-  gamma: number = 0;
+import { Gyro } from './gyro';
 
-  constructor() {
-    this.calibrateCenter();
-  }
+const _gyroRef = new Gyro();
+let _alpha: HTMLElement | null;
+let _beta: HTMLElement | null;
+let _gamma: HTMLElement | null;
 
-  calibrateCenter() {
-    window.addEventListener(
-      'deviceorientation',
-      (e: DeviceOrientationEvent) => {
-        this.alpha = e.alpha ?? 0;
-        this.beta = e.beta ?? 0;
-        this.gamma = e.gamma ?? 0;
-      },
-      { once: true }
-    );
-  }
-}
-
-type VirtualKeyboard = {
-  boundingRect: Readonly<DOMRect>;
-  overlaysContent: boolean;
-  show: () => void;
-  hide: () => void;
-};
-
-const _gyroRef = new GyroReference();
 window.onload = () => {
+  // Setup
+  _alpha = document.getElementById('alpha');
+  _beta = document.getElementById('beta');
+  _gamma = document.getElementById('gamma');
   if (window.DeviceOrientationEvent) {
     window.addEventListener(
       'deviceorientation',
       (e: DeviceOrientationEvent) => {
-        let alpha = document.getElementById('alpha');
-        let beta = document.getElementById('beta');
-        let gamma = document.getElementById('gamma');
-
-        if (alpha && beta && gamma && e.alpha && e.beta && e.gamma) {
-          alpha.innerText = e.alpha - _gyroRef.alpha + '';
-          beta.innerText = e.beta - _gyroRef.beta + '';
-          gamma.innerText = e.gamma - _gyroRef.gamma + '';
+        if (_alpha && _beta && _gamma && e.alpha && e.beta && e.gamma) {
+          _alpha.innerText = e.alpha - _gyroRef.alpha + '';
+          _beta.innerText = e.beta - _gyroRef.beta + '';
+          _gamma.innerText = e.gamma - _gyroRef.gamma + '';
         }
       },
       false
